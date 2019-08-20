170e88d89e2108d25b66cb0c0e0028ea089f6b58
Alvaro Vidal <Alvaro Vidal@DesarrolloMB.local>
2019-08-20 16:39:38 -0400

    TEST Methods added

--- controllers/default.js
+++ controllers/default.js
@@ -4,10 +4,43 @@ var express = require('express'),
     app = express(),
     router = express.Router();
 
-app.post('/', () => {});
+//app.post('/', () => {});
 
-router.post('/', () => {
 
+
+router.get('/test', (req, res) => {
+
+    res.status(200).json({
+        ok: true,
+        message: "Este GET se ve bueno chicos"
+    });
+
+});
+
+router.post('/test', (req, res) => {
+
+    res.status(200).json({
+        ok: true,
+        message: "Este POST se ve bueno chicos"
+    });
+
+});
+
+router.put('/test', (req, res) => {
+
+    res.status(200).json({
+        ok: true,
+        message: "Este PUT se ve bueno chicos"
+    });
+
+});
+
+router.delete('/test', (req, res) => {
+
+    res.status(200).json({
+        ok: true,
+        message: "Este DELETE se ve bueno chicos"
+    });
 
 });
 
